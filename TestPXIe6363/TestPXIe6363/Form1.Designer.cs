99ad17c1ca36147b8888845c2f8e1009258c22c0
jChambord <70707261+jeromeChamb@users.noreply.github.com>
2025-06-27 16:10:07 +0200

    Add files via upload

---
+++ TestPXIe6363/TestPXIe6363/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TestPXIe6363
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.valueDisplay = new System.Windows.Forms.TextBox();
+            this.buttonLire = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valeur mesurée : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // valueDisplay
+            // 
+            this.valueDisplay.Location = new System.Drawing.Point(125, 28);
+            this.valueDisplay.Name = "valueDisplay";
+            this.valueDisplay.Size = new System.Drawing.Size(88, 22);
+            this.valueDisplay.TabIndex = 1;
+            this.valueDisplay.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonLire
+            // 
+            this.buttonLire.Location = new System.Drawing.Point(260, 30);
+            this.buttonLire.Name = "buttonLire";
+            this.buttonLire.Size = new System.Drawing.Size(75, 23);
+            this.buttonLire.TabIndex = 2;
+            this.buttonLire.Text = "Lire";
+            this.buttonLire.UseVisualStyleBackColor = true;
+            this.buttonLire.Click += new System.EventHandler(this.buttonLire_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 79);
+            this.Controls.Add(this.buttonLire);
+            this.Controls.Add(this.valueDisplay);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PXIe-6363 Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox valueDisplay;
+        private System.Windows.Forms.Button buttonLire;
+    }
+}
+
---
+++ TestPXIe6363/TestPXIe6363/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments.DAQmx;
+
+using Task = NationalInstruments.DAQmx.Task;
+
+namespace TestPXIe6363
+{
+    public partial class Form1 : Form
+    {
+        private Task task;
+        private AnalogSingleChannelReader analogSingleChannelReader; // lecteur 
+        private CancellationTokenSource cancellationTokenSource;  // Source pour annuler la tâche du 2eme thread
+        private bool isTaskRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLire_Click(object sender, EventArgs e)
+        {
+            if (!isTaskRunning)
+            {
+                try
+                {
+                    task = new Task();
+
+                    // Création du canal d'acquisition
+                    task.AIChannels.CreateVoltageChannel(
+                            "PXI1Slot2/ai0", // nom exact dans NI MAX
+                            "Channel",
+                            AITerminalConfiguration.Differential, // possible RSE avec masse commune demandé explication
+                            -10, 10, // Plage de tension en volts
+                            AIVoltageUnits.Volts
+                        );
+
+                    // Configure la tâche pour lire de manière continue
+                    task.Timing.ConfigureSampleClock(
+                        "",
+                        1000, // Fréquence d'échantillonnage (1 kHz)
+                        SampleClockActiveEdge.Rising,           // à voir 
+                        SampleQuantityMode.ContinuousSamples,   // à voir 
+                        1000 // Taille du buffer pour les échantillons continus
+                    );
+
+                    // Crée le lecteur de données pour lire à partir du stream de la tâche
+                    analogSingleChannelReader = new AnalogSingleChannelReader(task.Stream);
+
+                    // Démarre la tâche d'acquisition
+                    task.Start();
+
+                    isTaskRunning = true;
+
+                    // Crée le token de cancellation pour arrêter la tâche proprement
+                    cancellationTokenSource = new CancellationTokenSource();
+                    CancellationToken token = cancellationTokenSource.Token;
+
+                    // Démarre une tâche asynchrone pour la lecture continue
+                    System.Threading.Tasks.Task.Run(() => ContinuouslyReadData(token));
+
+                }
+                catch (DaqException ex)
+                {
+                    MessageBox.Show("Erreur DAQmx : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    // Annuler la tâche qui tourne sur le 2eme thread
+                    cancellationTokenSource.Cancel();
+
+                    // Arrête la tâche d'acquisition continue
+                    task.Stop();
+                    task.Dispose();
+
+                    // Met à jour l'état de la tâche
+                    isTaskRunning = false;
+                    valueDisplay.Text = "Arrêté"; // Affichage de l'état d'arrêt
+                }
+                catch (DaqException ex)
+                {
+                    MessageBox.Show("Erreur lors de l'arrêt de la tâche : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ContinuouslyReadData(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    // Lit une seule valeur (à partir du buffer)
+                    double valeur = analogSingleChannelReader.ReadSingleSample();
+
+                    // Met à jour l'interface utilisateur sur le thread principal
+                    Invoke(new Action(() =>
+                    {
+                        // Affiche la dernière valeur lue dans un Label
+                        valueDisplay.Text = valeur.ToString("0.000") + " V";
+                    }));
+
+                    // Attendre un peu avant de lire à nouveau pour ne pas surcharger l'interface utilisateur
+                    System.Threading.Thread.Sleep(100); // Pause de 100 ms
+                }
+            }
+            catch (DaqException ex)
+            {
+                MessageBox.Show("Erreur pendant la lecture continue : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
